bc4bf4d88566f3e8f058a9e27ebf26da00143774
ezzeldem <ezzelden06@gmail.com>
2023-02-16 21:06:26 +0200

    new conponents

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -28,39 +28,22 @@ $(document).ready(function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    // on: {
-    //   slideChangeTransitionEnd: function () {
-    //     let swiper = this,
-    //       captions = swiper.el.querySelectorAll(".caption");
-    //     // for (let i = 0; i < captions.length; ++i) {
-    //     //   captions[i].classList.remove('show');
-    //     // }
-    //     //swiper.slides[swiper.activeIndex].querySelector('.caption').classList.add('show');
-    //   },
-    //   progress: function (data) {
-    //     let swiper = this;
-    //     for (let i = 0; i < swiper.slides.length; i++) {
-    //       let slideProgress = swiper.slides[i].progress,
-    //         innerOffset = swiper.width * interleaveOffset,
-    //         innerTranslate = slideProgress * innerOffset;
-    //       swiper.slides[i].querySelector(".slide-bgimg").style.transform =
-    //         "translateX(" + innerTranslate + "px)";
-    //     }
-    //   },
-    //   touchStart: function () {
-    //     let swiper = this;
-    //     for (let i = 0; i < swiper.slides.length; i++) {
-    //       swiper.slides[i].style.transition = "";
-    //     }
-    //   },
-    //   setTransition: function (speed) {
-    //     let swiper = this;
-    //     for (let i = 0; i < swiper.slides.length; i++) {
-    //       swiper.slides[i].style.transition = speed + "ms";
-    //       swiper.slides[i].querySelector(".slide-bgimg").style.transition =
-    //         speed + "ms";
-    //     }
-    //   },
-    // },
+  });
+
+  var swiper2 = new Swiper(".projects-slider", {
+    grabCursor: true,
+    slidesPerView: 2,
+    loop: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: ["-120%", 0, -500],
+      },
+      next: {
+        shadow: true,
+        translate: ["120%", 0, -500],
+      },
+    },
   });
 });
